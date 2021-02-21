a0fda04507bc283acf1a122415c862e80801f075
Aaron Lin <aaronlin256@gmail.com>
2021-02-20 23:51:27 -0500

    add project'

---
+++ README.md
@@ -0,0 +1,7 @@
+# Alphabet Position
+
+![](./assets/images/en-US_largeIconUri.png)
+
+Alexa skill to let users learn position of letters in the English alphabet  
+
+Template from high low game  
---
+++ lambda/languageStrings.js
@@ -0,0 +1,20 @@
+module.exports = {
+  translation: {
+    SKILL_NAME: 'Alphabet Position Game',
+    EXIT_MESSAGE: 'Thanks for playing!',
+    FALLBACK_MESSAGE_DURING_GAME: `I can't help you with that.  Try guessing a number between 1 and 26. `,
+    FALLBACK_REPROMPT_DURING_GAME: 'Please guess a number between 1 and 26.',
+    FALLBACK_MESSAGE_OUTSIDE_GAME: `I can't help you with that.  I will come up with a letter from the English alphabet and you try to tell me the position of the letter from 1 to 26. Would you like to play?`,
+    GUESS_CORRECT_MESSAGE: '%s is correct! Would you like to play again?',
+    LAUNCH_MESSAGE: 'Welcome to Alphabet Position game. You have played %s times. Would you like to play?',
+    TOO_HIGH_MESSAGE: '%s is too high.',
+    TOO_HIGH_REPROMPT: 'Try a smaller number.',
+    TOO_LOW_MESSAGE: '%s is too low.',
+    TOO_LOW_REPROMPT: 'Try a larger number.',
+    CONTINUE_MESSAGE: 'Say yes to play or no to quit.',
+    HELP_MESSAGE: 'I am going to state a letter from the English alphabet. Tell me what position is the letter between one and twenty-six, try telling me a position number and I will tell you if it is higher or lower.',
+    HELP_REPROMPT: 'Try saying a number.',
+    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    YES_MESSAGE: 'Great! Tell me the position for the letter: %s'
+  }
+};
